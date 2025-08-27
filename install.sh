815bf1a3521eaf9ba914d43509578bac308738cb
Admin <a.ross1@hotmail.com>
2025-08-27 12:21:27 +1000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# VPSmith installer – Created by Digital Platform Online Studios
+set -e
+echo "=== VPSmith installer ==="
+echo "Domain to use (or IP):"
+read DOMAIN
+echo "You entered: $DOMAIN"
+# (add real docker-compose pulls, SSL, etc. here)
